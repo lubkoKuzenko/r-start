d1d08bacc889367b32a1fa71c613efbdc59464b2
Lyubomyr Kuzenko <lyubomyrkuzenko@coherentsolutions.com>
2023-01-24 17:33:28 +0200

    ref

--- src/pages/Counter/Counter.tsx
+++ src/pages/Counter/Counter.tsx
@@ -4,7 +4,6 @@ import { usePrevious } from '../../utils/hooks';
 
 const Counter: React.FC = () => {
   const { t } = useTranslation();
-
   const [count, setCount] = useState(0);
   const prevCount = usePrevious(count);
 
